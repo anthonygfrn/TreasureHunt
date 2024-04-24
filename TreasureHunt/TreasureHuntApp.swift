fb234e2caf8f6aa2ec0a612531454a37a2cf6c85
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-04-24 12:45:29 +0700

    Initial Commit

---
+++ TreasureHunt/TreasureHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureHuntApp.swift
+//  TreasureHunt
+//
+//  Created by Anthony on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TreasureHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
