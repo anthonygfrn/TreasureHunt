6248f640e4e9a456814acc2f9e517ba4c5587129
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-04-29 09:59:58 +0700

    feat: background audio

--- TreasureHunt/TreasureHuntView.swift
+++ TreasureHunt/TreasureHuntView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct TreasureHuntView: View {
     var body: some View {
         NavigationStack {
                 LandingPage()
         }
+        .onAppear {
+                 // Start playing background audio
+                 AudioManager.shared.playBackgroundMusic()
+             }
     }
+}
+
+class AudioManager {
+ static let shared = AudioManager()
+ 
+ var audioPlayer: AVAudioPlayer?
+
+ func playBackgroundMusic() {
+     if let url = Bundle.main.url(forResource: "backgroundMusic", withExtension: "mp3") {
+         do {
+             audioPlayer = try AVAudioPlayer(contentsOf: url)
+             audioPlayer?.numberOfLoops = -1 // Set to loop indefinitely
+             audioPlayer?.play()
+         } catch {
+             print("Error playing background music: \(error.localizedDescription)")
+         }
+     } else {
+         print("Background music file not found")
+     }
+ }
 }
 
 #Preview {
